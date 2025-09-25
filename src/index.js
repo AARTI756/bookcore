6ad4f0e66df79ec226f5afd73f5893571dfc863f
Aarti <asakpal756@gmail.com>
2025-09-25 19:50:33 +0530

    Fix: Update Google OAuth redirect URIs for Vercel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Basic reset styles
 import './App.css';   // Global app styles and variables
 import App from './App';
 
